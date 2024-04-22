37e19dbe2edcffe3f9acab94eb68438ce302ff5f
huy.nguyenquang <huy.nguyenquang@evotek.vn>
2024-04-22 23:35:31 +0700

    add firebase

--- angular/demo1/src/app/modules/users/users.module.ts
+++ angular/demo1/src/app/modules/users/users.module.ts
@@ -2,16 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersListComponent } from './users-list/users-list.component';
 import { UserCommandComponent } from './user-command/user-command.component';
+import {RouterModule, Routes} from "@angular/router";
+import {DropdownMenusModule, WidgetsModule} from "../../_metronic/partials";
+import {InlineSVGModule} from "ng-inline-svg";
 
-
+const routes: Routes = [{
+  path: '',
+  component: UsersListComponent,
+},
+  {
+    path: 'add',
+    component: UserCommandComponent,
+  }
+]
 
 @NgModule({
   declarations: [
     UsersListComponent,
-    UserCommandComponent
+    UserCommandComponent,
+
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    WidgetsModule,
+    DropdownMenusModule,
+    InlineSVGModule
   ]
 })
 export class UsersModule { }
